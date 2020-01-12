c80479526f8c6c63d9b5fa17f4dcec5762a65922
travis skyles <travis.skyles@gmail.com>
2020-01-12 14:54:56 -0800

    mongo-model scaffolded

---
+++ models/mongo-model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let 
+
+/**
+ * @class Model
+ */
+
+ class Model {
+   constructor(){
+    this.schema = schema;
+   }
+
+   // get record based on id
+   get(_id){
+
+   }
+
+   create(record){
+
+   }
+
+   update(_id, record){
+
+   }
+
+   delete(_id){
+
+   }
+ }
