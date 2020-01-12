0ba92ab1a5f47582343c865d8c9e859cc883cdcd
travis skyles <travis.skyles@gmail.com>
2020-01-12 15:16:34 -0800

    added documentation to models

--- models/categories-model.js
+++ models/categories-model.js
@@ -2,12 +2,17 @@
 
 const dataModel = require('../models/mongo-model');
 const schema = require('../schema/categories-schema');
-
+/**
+ * @class Categories
+ * @extends {dataModel}
+ */
 class Categories extends dataModel{
   constructor(){
     super();
     this.schema = schema;
   }
 }
-
+/**
+ * @module Categories
+ */
 module.exports = Categories;
--- models/products-model.js
+++ models/products-model.js
@@ -2,12 +2,17 @@
 
 const dataModel = require('../models/mongo-model');
 const schema = require('../schema/products-schema');
-
+/**
+ * @class Products
+ * @extends {dataModel}
+ */
 class Products extends dataModel{
   constructor(){
     super();
     this.schema = schema;
   }
 }
-
+/** 
+ * @module Products
+*/
 module.exports = Products;
