1a325c17967d6741862d0358631d1cfb023edeb4
travis skyles <travis.skyles@gmail.com>
2020-01-12 15:13:38 -0800

    created categories-model

---
+++ models/categories-model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const dataModel = require('../models/mongo-model');
+const schema = require('../schema/categories-schema');
+
+class Categories extends dataModel{
+  constructor(){
+    super();
+    this.schema = schema;
+  }
+}
