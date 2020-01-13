a52b8a578b504ed4c53fabac7ed63777e609ed85
travis skyles <travis.skyles@gmail.com>
2020-01-12 21:04:52 -0800

    fleshed out mongo-model

--- models/mongo-model.js
+++ models/mongo-model.js
@@ -1,30 +1,49 @@
 'use strict';
 
-let 
-
 /**
  * @class Model
  */
 
- class Model {
-   constructor(){
+class Model {
+  constructor(){
     this.schema = schema;
-   }
+  }
 
-   // get record based on id
-   get(_id){
+  // get record based on id
+  get(_id){
+    if(_id){
+      return this.schema.findOne( {_id} );
+    }
+    else this.schema.find();
+  }
 
-   }
+  create(record){
+    if(typeof record === 'object'){
+      let newObject = this.schema(record);
+      return newObject.save();
+    }
+    else {
+      return Error('something went wrong.. Could not save to db..');}
+  }
+  
 
-   create(record){
+  update(_id, record){
+    if(_id && record){
+      return this.schema.findOneAndUpdate(record);
+    }
+    else {
+      return undefined;
+    }
+  }
 
-   }
+  delete(_id){
+    if(_id){
+      return this.schema.findByIdAndDelete(_id);
+    }
+    else {
+      return 'could not find record...';
+    }
+  }
+}
 
-   update(_id, record){
-
-   }
-
-   delete(_id){
-
-   }
- }
+module.exports = Model;
