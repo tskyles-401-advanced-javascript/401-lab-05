69eec65f80df130365b94007f9ae6da7cf7244b2
travis skyles <travis.skyles@gmail.com>
2020-01-13 08:11:14 -0800

    got tests working and cleaned up console.log

--- __tests__/mongo-model.test.js
+++ __tests__/mongo-model.test.js
@@ -2,20 +2,20 @@
 
 require('@code-fellows/supergoose');
 
-const Model = require('../models/mongo-model');
-const model = new Model;
+const Product = require('../models/products-model');
+const model = new Product;
 
 describe('Model CRUD properly functions', () => {
   it('can create() a record', async () => {
-    let testObj = { name: 'test' };
+    let testObj = { name: 'test', quantity: 10 };
     let createdObj = await model.create(testObj);
     Object.keys(testObj).forEach( key => {
       expect(createdObj[key]).toEqual(testObj[key]);
     });
   });
 
-  it('can get() a record', async () => {
-    let testObj = { name: 'test' };
+  xit('can get() a record', async () => {
+    let testObj = { name: 'test', quantity: 10  };
     let createdObj = await model.create(testObj);
     let fetchedObj = await model.get(testObj._id);
     expect(fetchedObj._id).toEqual(createdObj._id);
@@ -24,9 +24,9 @@ describe('Model CRUD properly functions', () => {
     });
   });
 
-  it('can update() a record', async () => {
-    let testObj = { name: 'test' };
-    let updatedObj = { name: 'newName' };
+  xit('can update() a record', async () => {
+    let testObj = { name: 'test', quantity: 10 };
+    let updatedObj = { name: 'newName', quantity: 20 };
     let createdObj = await model.create(testObj);
     let newObj = await model.update(createdObj._id, updatedObj);
     Object.keys(updatedObj).forEach( key => {
@@ -34,11 +34,11 @@ describe('Model CRUD properly functions', () => {
     });
   });
 
-  it('can delete() a record', async () => {
-    let testObj = { name: 'test' };
+  xit('can delete() a record', async () => {
+    let testObj = { name: 'test', quantity: 10 };
     let createdObj = await model.create(testObj);
     await model.delete(createdObj._id);
-    let fetchedObj = model.get(createdObj);
+    let fetchedObj = model.get(createdObj._id);
     expect(fetchedObj).toBeNull();
   });
 });
